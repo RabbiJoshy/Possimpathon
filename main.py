96df17b905e297346661c7da9bb461627f1204c6
JT <joshuasthomas@me.com>
2023-01-05 23:33:26 +0100

    update possim

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.preprocessing import MinMaxScaler
 from Utils import IO
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv('db.csv')
-run = 10
+#TODO - 2 values have effective depth
+rawdf = pd.read_csv('db.csv')
+df = rawdf.dropna()
+df = df.reset_index()
+
+
+rawdf.iloc[192]
 
 def dispdaylight(df, run):
  daylightimage = np.array(df['out:DFdata'][run].split(';'))#.reshape(16,6)
@@ -30,12 +38,12 @@ scaler = MinMaxScaler()
 data_notscaled = df.drop(inputs, axis=1)
 df_scaled = scaler.fit_transform(df[inputs].to_numpy())
 df_scaled = pd.DataFrame(df_scaled, columns= inputs)
-df_norm = pd.concat((df_scaled, data_notscaled), 1)
+df_norm = pd.concat((df_scaled, data_notscaled), axis = 1)
 
 
 train, test = train_test_split(df_norm, test_size= 0.8, random_state = 69)
 
-model = xgb.XGBRegressor()
+model = LinearRegression()#xgb.XGBRegressor()
 model.fit(train[inputs], train['out:Effectivedepth'])
 y_pred = model.predict(test[inputs])
 y_true = test['out:Effectivedepth']
@@ -43,10 +51,16 @@ MAE = mean_absolute_error(y_true, y_pred)
 
 test['error'] = abs(y_pred-y_true)
 test['pred'] = y_pred
-view = pd.DataFrame(dic)
-view.set_index()
+
 
 imagefor = test.sort_values(by = 'error', ascending = False).iloc[:20, :]
 imagefor_index = list(imagefor.index)
 for run in imagefor_index:
  dispdaylight(df, run)
+
+ df['out:SVdata'][0]
+
+df['out:SVdata'] = df['out:SVdata'].apply(lambda x: x.split(';'))
+
+hmm = df['out:SVdata']
+jniigek = hmm.loc[0]
